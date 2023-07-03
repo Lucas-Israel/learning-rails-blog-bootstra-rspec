96aba47fc8b90a97d5783df96a5320ab3d1fc470
Lucas Israel <lisraelalves92@gmail.com>
2023-07-03 13:06:10 -0300

    feat: image for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,6 +50,6 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :body, :status)
+    params.require(:article).permit(:title, :body, :status, :cover_url)
   end
 end
